13a396361552579ef5d6c656093ac033ee55c104
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2023-12-24 17:05:38 +0100

    Add project files.

---
+++ LeaveManagement/Data/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Data
+{
+    public class Employee:IdentityUser
+    {
+        public string ? Firstname {  get; set; }
+        public string? Lastname { get; set; }
+        
+        public string ?TaxId {  get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+
+
+    }
+}
---
+++ LeaveManagement/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Data
+{
+    public class LeaveType:BaseEntity
+    {
+
+        public string Name { get; set; }
+
+        public int DefaultDays {  get; set; }
+
+        
+    }
+}
